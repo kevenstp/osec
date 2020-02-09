e73720fdcdd16f547445c1157deb1baeed8b4652
MtnDewd96 <Mm4503488>
2020-02-08 20:30:19 -0500

    Billboard broker basique

---
+++ app/Controllers/BillboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Controllers;
+
+
+class BillboardController extends Controller
+{
+
+    public function initializeRoutes()
+    {
+        $this->get("/babillard", "renderBillboard");
+        $this->get("/babillard/creation", ""); //todo
+    }
+
+    public function renderBillboard()
+    {
+
+    }
+}
